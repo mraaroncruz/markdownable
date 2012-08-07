ec32de23afa9429e31085b3e2af201feaab59e35
Aaron Cruz <aaron@aaroncruz.com>
2012-08-07 13:54:46 +0200

    Added guts of module

--- lib/markdownable.rb
+++ lib/markdownable.rb
@@ -1,5 +1,23 @@
 require "markdownable/version"
 
 module Markdownable
-  # Your code goes here...
+  module ClassMethods
+    def markdownable field
+      define_method "#{field}_markdown" do
+        renderer = Redcarpet::Render::HTML.new(:hard_wrap => true)
+        markdown = Redcarpet::Markdown.new(renderer,
+                                     :autolink => true,
+                                     :lax_html_blocks => true)
+        markdown.render(self.send field)
+      end
+    end
+  end
+
+  def self.included(receiver)
+    receiver.extend ClassMethods
+  end
+end
+
+if defined? ActiveRecord::Base
+  ActiveRecord::Base.send :include, Markdownable
 end
