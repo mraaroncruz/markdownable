22e615e9f685620325a5dd4d85318f76449767cb
Aaron Cruz <aaron@aaroncruz.com>
2012-08-07 15:00:37 +0200

    Specs for multiple args and nil property on PORO

--- lib/markdownable.rb
+++ lib/markdownable.rb
@@ -3,14 +3,16 @@ require 'markdownable/version'
 
 module Markdownable
   module ClassMethods
-    def markdownable field
-      define_method "#{field}_markdown" do
-        renderer = Redcarpet::Render::HTML.new(:hard_wrap => true)
-        markdown = Redcarpet::Markdown.new(renderer,
-                                     :autolink => true,
-                                     :lax_html_blocks => true)
-        text = self.send(field)
-        markdown.render(text) unless text.nil?
+    def markdownable(*fields)
+      fields.each do |field|
+        define_method "#{field}_markdown" do
+          renderer = Redcarpet::Render::HTML.new(:hard_wrap => true)
+          markdown = Redcarpet::Markdown.new(renderer,
+                                       :autolink => true,
+                                       :lax_html_blocks => true)
+          text = self.send(field)
+          markdown.render(text) unless text.nil?
+        end
       end
     end
   end
--- spec/poro_spec.rb
+++ spec/poro_spec.rb
@@ -5,16 +5,21 @@ require 'rspec/autorun'
 class Article
   include Markdownable
   attr_accessor :title, :author, :body
-  markdownable :body
+  markdownable :body, :title
 end
 
 describe "PORO with markdownable" do
   before(:each) do
     @article = Article.new
-    @article.title = "My Article"
+    @article.title  = nil
     @article.author = "Aaron Cruz"
-    @article.body = "## Hello world"
+    @article.body   = "## Hello world"
   end
   subject { @article }
   its(:body_markdown) { should =~ /<h2>Hello world<\/h2>/ }
+
+  it "should return nil for nil property" do
+    @article.title.should be_nil
+    @article.title_markdown.should be_nil
+  end
 end
